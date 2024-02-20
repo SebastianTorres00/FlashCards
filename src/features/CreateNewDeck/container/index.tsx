c0fd3bba2fd5c0f2bf50c406c22c4a6f502ced50
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2024-02-20 09:15:00 -0300

    feat: seleccion de pasos

--- src/features/CreateNewDeck/container/index.tsx
+++ src/features/CreateNewDeck/container/index.tsx
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { CreateDeck } from '../components/';
+import useCreateCards from '../hooks/useCreateCards';
+import CreateDeckSecondStep from '../components/CreateDeckSecondStep';
+import { useNavigation } from '@react-navigation/native';
 
 const CreateDeckScreen = () => {
+	const {
+		titleCard,
+		onChangeTitleCards,
+		step,
+		onChangeSteps,
+		listTextInput,
+		createMoreInputs,
+		onChangeResponse,
+		onSubmitCard,
+		onChangeTitle,
+		onShowModal,
+	} = useCreateCards();
+	const navigation = useNavigation();
 	return (
 		<View style={styles.container}>
-			<CreateDeck />
-			<StatusBar style="auto" />
+			{(() => {
+				switch (step) {
+					case '1':
+						return (
+							<CreateDeck
+								titleCard={titleCard}
+								onChangeTitleCards={onChangeTitleCards}
+								onChangeSteps={onChangeSteps}
+								navigation={navigation}
+							/>
+						);
+					default:
+						return (
+							<CreateDeckSecondStep
+								titleCard={titleCard}
+								onChangeSteps={onChangeSteps}
+								listTextInput={listTextInput}
+								createMoreInputs={createMoreInputs}
+								onChangeResponse={onChangeResponse}
+								onSubmitCard={onSubmitCard}
+								onChangeTitle={onChangeTitle}
+								onShowModal={onShowModal}
+							/>
+						);
+				}
+			})()}
 		</View>
 	);
 };
