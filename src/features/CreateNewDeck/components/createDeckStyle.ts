3fbc36d5f8bbd665a0dbbf3137447cfb9fe76b05
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2024-02-20 09:16:03 -0300

    feat: crear style

---
+++ src/features/CreateNewDeck/components/createDeckStyle.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const createDeckStyle = StyleSheet.create({
+	container: {
+		backgroundColor: 'steelblue',
+		width: '100%',
+		height: '100%',
+		padding: 10,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
+
+export default createDeckStyle;
