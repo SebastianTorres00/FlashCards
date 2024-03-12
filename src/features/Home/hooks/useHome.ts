a2d45e8215e157ad40e382394f3cb93a4497ee32
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2024-03-12 12:30:43 -0300

    feat: hook mejora

--- src/features/Home/hooks/useHome.ts
+++ src/features/Home/hooks/useHome.ts
@@ -1,14 +1,28 @@
 import { useSelector } from 'react-redux';
 import { IStateListCard } from '../../../types';
 import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 
 const useHome = () => {
-	const { cards } = useSelector((state: IStateListCard) => state.listCard);
+	const cards = useSelector((state) => state.listCard.cards[0]);
+	const cardsListArr: never[] = [];
+	const [cardsList, setCardsList] = useState([]);
+	const [lengthCards, setLengthCards] = useState(0);
+	useEffect(() => {
+		for (const property in cards) {
+			cardsListArr.push({
+				[property]: cards[property],
+			});
+		}
+		setLengthCards(cardsListArr.length);
+		setCardsList(cardsListArr);
+	}, [cards]);
+
 	const navigation = useNavigation();
 	const onPressBtn = () => {
 		navigation.navigate('CreateDeck');
 	};
-	return { cards, onPressBtn };
+	return { cardsList, onPressBtn, lengthCards };
 };
 
 export default useHome;
