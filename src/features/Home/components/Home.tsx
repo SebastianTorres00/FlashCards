0c6b1f475cad159473725d544008efedb0d684fe
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2024-03-12 12:29:52 -0300

    feat: map cards

--- src/features/Home/components/Home.tsx
+++ src/features/Home/components/Home.tsx
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Text } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { IPropsUseHome } from '../types';
 
-const Home = ({ cards, onPressBtn }: IPropsUseHome) => {
+const Home = ({ cardsList, onPressBtn, lengthCards }: IPropsUseHome) => {
+	// console.log('Home LOG ---> cardsList', cardsList);
+	// console.log('LOG ---> lengthCards', lengthCards);
 	return (
 		<>
-			<Text style={{ fontSize: 20 }}>You have {cards?.length} cards</Text>
-			<Button onPress={onPressBtn} title="text" />
-			<StatusBar style="auto" />
+			<View>
+				<Text style={{ fontSize: 20 }}>You have {lengthCards} cards</Text>
+				{cardsList?.map((item) => {
+					console.log('test ----> Object.keys(item)[0]', Object.keys(item)[0]);
+					return (
+						<View
+							key={Object.keys(item)[0]}
+							style={{ backgroundColor: 'green' }}
+						>
+							<Text>{Object.keys(item)[0]}</Text>
+						</View>
+					);
+				})}
+
+				<Button onPress={onPressBtn} title="text" />
+				<StatusBar style="auto" />
+			</View>
 		</>
 	);
 };
