f465c71f6825fe9a06dc76e4c00f405b06f88c4f
SebastianTorres00 <sebastiantorrescontacto@gmail.com>
2024-03-13 21:47:35 -0300

    feat: libreria toaster

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import { PaperProvider } from 'react-native-paper';
 import Navigation from './src/navigation';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import { ToastProvider } from 'react-native-paper-toast';
 
 const App = () => {
 	return (
 		<Provider store={store}>
 			<PaperProvider>
-				<Navigation />
+				<ToastProvider>
+					<Navigation />
+				</ToastProvider>
 			</PaperProvider>
 		</Provider>
 	);
